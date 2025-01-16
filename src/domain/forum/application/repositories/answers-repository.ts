4d693de2903f33253edcae7a874a62552ac55b79
Pedro Henrique <pedrohenriquedarck@hotmnail.com>
2025-01-16 20:11:18 -0300

    Feat: Delete Answers

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,5 +1,7 @@
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
+  findbyId(id: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
+  delete(answer: Answer): Promise<void>
 }
---
+++ src/domain/forum/application/uses-cases/delete-answer.spec.ts
@@ -0,0 +1,50 @@
+import { InMemoryAnswersRepository } from 'test/repositorys/in-memory-answers-repository'
+import { DeleteAnswerUseCase } from './delete-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswer } from 'test/factories/make-answer'
+
+let inMemoryAnswerRepository: InMemoryAnswersRepository
+let sut: DeleteAnswerUseCase
+
+describe('Delete Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswersRepository()
+    sut = new DeleteAnswerUseCase(inMemoryAnswerRepository)
+  })
+
+  it('Should be able to delete a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswerRepository.create(newAnswer)
+
+    await sut.execute({
+      answerId: 'answer-1',
+      authorId: 'author-1',
+    })
+
+    expect(inMemoryAnswerRepository.items).toHaveLength(0)
+  })
+
+  it('Should not be able to delete a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswerRepository.create(newAnswer)
+
+    expect(() => {
+      return sut.execute({
+        answerId: 'answer-1',
+        authorId: 'author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
