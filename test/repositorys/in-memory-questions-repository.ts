eef0ddb6eac23fb7a07c563fe7d6cc446a461c71
Pedro Henrique <pedrohenriquedarck@hotmnail.com>
2025-01-16 21:43:09 -0300

    Feat: fetch recent questions

--- test/repositorys/in-memory-questions-repository.ts
+++ test/repositorys/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
@@ -28,6 +29,16 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => {
+        return b.createdAt.getTime() - a.createdAt.getTime()
+      })
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 
   async create(question: Question) {
