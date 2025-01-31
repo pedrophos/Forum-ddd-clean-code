f23b6075613f15c1bc7859bebbb1c48d623e9faa
Pedro Henrique <pedrohenriquedarck@hotmnail.com>
2025-01-30 21:46:55 -0300

    Refactor: Type error

--- src/domain/forum/application/uses-cases/get-question-by-slug.ts
+++ src/domain/forum/application/uses-cases/get-question-by-slug.ts
@@ -1,12 +1,16 @@
-import { Either, right } from '@/core/either'
+import { Either, left, right } from '@/core/either'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/question-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
 }
 
-type GetQuestionBySlugUseCaseResponse = Either<null, { question: Question }>
+type GetQuestionBySlugUseCaseResponse = Either<
+  ResourceNotFoundError,
+  { question: Question }
+>
 
 export class GetQuestionBySlugUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
@@ -17,7 +21,7 @@ export class GetQuestionBySlugUseCase {
     const question = await this.questionsRepository.findBySlug(slug)
 
     if (!question) {
-      throw new Error('Question not found.')
+      return left(new ResourceNotFoundError())
     }
 
     return right({ question })
