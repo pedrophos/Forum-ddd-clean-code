9672363206feb43bd841af38483c7d25316b4fa9
Pedro Henrique <pedrohenriquedarck@hotmnail.com>
2025-02-06 21:50:06 -0300

    Feat: attachments on questions

--- src/domain/forum/application/uses-cases/create-question.spec.ts
+++ src/domain/forum/application/uses-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test/repositorys/in-memory-questions-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionRepository: InMemoryQuestionsRepository
 let sut: CreateQuestionUseCase
@@ -15,9 +16,15 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'Question Title',
       content: 'Question content',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBeTruthy()
     expect(inMemoryQuestionRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
   })
 })
